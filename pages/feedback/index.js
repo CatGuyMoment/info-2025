48c6bdc6c443a8fe7a7ec4519d31e0e06d140498
CatGuyMoment <amirchik57@outlook.com>
2025-11-07 13:42:59 +0100

    made feedback thingy track success

---
+++ pages/feedback/index.js
@@ -0,0 +1,11 @@
+const params = new URLSearchParams(window.location.search);
+const success = params.get('success'); // Gets the value of the "name" parameter
+
+
+if (success === '0') {
+
+} else if (success === '1') {
+
+} else {
+    
+}
